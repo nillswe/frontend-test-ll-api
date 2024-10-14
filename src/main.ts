b475f76b2d30558f92b7a2cc631a9074d791592f
Nill <niltonrck@gmail.com>
2024-10-14 08:10:02 -0300

    feat: add fake authorization check

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,27 @@ import express from "express"
 import dotenv from "dotenv"
 import { products } from "./data.mock";
 
-
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+const authToken = 'fake-token-do-not-delete'
 
 app.use(express.static('public'))
 
+
+// Fake token validation middleware
+app.use(function (req, res, next) {
+  const authorization = req.headers.authorization
+
+  if(!authorization?.includes(authToken)) {
+    res.status(401).json({message: 'Not authorized'})
+  }
+
+  next();
+});
+
+// Routes
 
 app.get('/products', (req: Request, res: Response) => {
   res.status(200).json(products)
@@ -26,8 +39,6 @@ app.get('/products/:slug', (req: Request, res: Response) => {
 
   res.status(200).json(product)
 });
-
-
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
